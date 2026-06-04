c54c39643cd2d3c3e6587d7a30e9e40533e89172
088uiop <LI088uiop@outlook.com>
2026-06-04 09:22:09 +0800

    .

--- shaders/ITM_Optimization.glsl
+++ shaders/ITM_Optimization.glsl
@@ -9,14 +9,10 @@ vec4 hook() {
   float luma = dot(color.rgb, luma_coeff);
   vec3 chroma = color.rgb - luma;
 
-  float sigmoid = 1.0 / (1.0 + exp((0.5 - luma) * 12.0));
-  float min_sig = 1.0 / (1.0 + exp(6.0));
-  float max_sig = 1.0 / (1.0 + exp(-6.0));
-
   float luma_factor = 2.8 * pow(1.0 - min(luma / 0.5, 1.0), 8.0) + 1.0;
-  float chroma_factor = 0.6 * (sigmoid - min_sig) / (max_sig - min_sig) + 1.0;
-
   luma *= luma_factor;
+
+  float chroma_factor = 0.6 / (1.0 + exp((0.5 - luma) * 12)) + 1.0;
   chroma *= chroma_factor;
 
   return vec4(luma + chroma, color.a);
